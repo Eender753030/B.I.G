a357ba9aeb544e2e73ebd947c3bbfa7fa816424c
Eender753030 <ender303030@gmail.com>
2025-11-22 10:19:31 +0800

    fix: Remove the multiple check of initialize

--- src/main.c
+++ src/main.c
@@ -12,12 +12,16 @@ int main(int argc, char **argv) {
     if (argc < 2)
         InputError();
 
-    if (strncmp(argv[1], "init", 5) == 0)
+    if (strncmp(argv[1], "init", 5) == 0) {
+        if (argc == 2)
         cmd_init();
+        else
+            ErrorCustomMsg("Usage: big init\n");
+
+    } else if (check_init() == -1)
+        NotInitError();
 
     else if (strncmp(argv[1], "add", 4) == 0) {
-        if (check_init() == -1)
-            NotInitError();
         if (argc < 3) {
             ErrorCustomMsg(
                 "Usage: big add <filename or directory> <...>\n"
@@ -26,8 +30,6 @@ int main(int argc, char **argv) {
         cmd_add((size_t)argc - 2, (const char **)argv + 2);
 
     } else if (strncmp(argv[1], "commit", 7) == 0) {
-        if (check_init() == -1)
-            NotInitError();
         if (argc == 2)
             cmd_commit(NULL);
         else {
@@ -38,21 +40,18 @@ int main(int argc, char **argv) {
         }
 
     } else if (strncmp(argv[1], "log", 4) == 0) {
-        if (check_init() == -1)
-            NotInitError();
         if (argc == 2)
             cmd_log(NULL);
-        else {
-            for (int i = 2; i < argc; i++) {
-                if (*argv[i] == '-') {
-                    char *c = argv[i] + 1;
-                    if (*c >= '0' && *c <= '9') {
+        else if (argc == 3) {
+            char *c = argv[2] + 1;
+            if (*argv[2] == '-' && *c >= '0' && *c <= '9') {
                         for (; *c != '\0'; c++) {
                             if (*c < '0' || *c > '9')
-                                ErrorCustomMsg("'%s' is not a positive integer\n", argv[i] + 1);
+                        ErrorCustomMsg("'%s' is not a positive integer\n", argv[2] + 1);
                         }
-                        long amount = strtol(argv[i] + 1, NULL, 10);
+                long amount = strtol(argv[2] + 1, NULL, 10);
                         cmd_log(&amount);
+
                     } else
                         ErrorCustomMsg("Usage: big log [-<amount>]\n");
                 } else
