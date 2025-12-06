41abd794b791964281d3cc1af9fcaad5f678d0e4
Eender753030 <ender303030@gmail.com>
2025-12-06 20:35:27 +0800

    docs: Add comments for bst

--- include/ds/bst.h
+++ include/ds/bst.h
@@ -4,36 +4,64 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+// Type of BST node that does not provide structure detail for client
 typedef struct bst_node bst_node_t;
 
+// Type of BST that does not provide structure detail for client
 typedef struct bst bst_t;
 
+// Create and return a newly allocated node that with generic data
 bst_node_t* bst_node_create(void* data);
 
+// Create and return a new empty BST that require a comparsion function for binary search
 bst_t* bst_create(int8_t (*cmp_callback)(void*, void*));
 
+/* Create a new BST from a sorted list that can ensure search time is O(log n)
+ * Also require a comparsion function for binary search
+ */
 bst_t* bst_create_from_list(void** list, uint64_t list_size, int8_t (*cmp_callback)(void*, void*));
 
+/* Insert data into BST
+ * Caller can decide whether handle the equal data situation for passing function pointer
+ */
 void bst_insert(bst_t* bst, void* data, void (*equal_handle_callback)(void*, void*));
 
+// Search node by data
 bst_node_t* bst_search(bst_t* bst, void* data);
 
+/* Delete node by data
+ * Require free function to tell how to free data and it's contents (if it has)
+ */
 void bst_delete(bst_t* bst, void* data, void (*free_func)(void**));
 
+/* Check two BST is same or not
+ * Require a function to know they are the same
+ */
 bool is_same_bst(bst_t* bst1, bst_t* bst2, bool (*is_same_callback)(void*, void*));
 
+// Use inorder traversal to flatten BST to a sorted list
 void** bst_inorder_to_list(bst_t* bst);
 
+// Inorder traversal and can call caller's callback for passing data and args
 void bst_inorder_func(bst_t* bst, void (*callback)(void*, void*), void* args);
 
+/* Free single BST node
+ * Require free function to tell how to free data and it's contents (if it has)
+ */
 void bst_node_free(bst_node_t** node, void (*free_callback)(void**));
 
+/* Free whole BST
+ * Require free function to tell how to free data and it's contents (if it has)
+ */
 void bst_free(bst_t** bst, void (*free_callback)(void**));
 
+// Getter for get BST's root
 bst_node_t* bst_get_root(bst_t* bst);
 
+// Getter for get BST's node amount
 uint64_t bst_get_amount(bst_t* bst);
 
+// Getter for get BST node's data
 void* bst_node_get_data(bst_node_t* node);
 
 #endif
--- src/ds/bst.c
+++ src/ds/bst.c
@@ -6,16 +6,17 @@
 #include "ds/stack.h"
 #include "utils/memory.h"
 
+/* BST node and BST structure implementaion */
 struct bst_node {
-    void* data;
+    void* data;  // Generic data
     struct bst_node* left;
     struct bst_node* right;
 };
 
 struct bst {
     bst_node_t* root;
-    uint64_t amount;
-    int8_t (*cmp_callback)(void*, void*);
+    uint64_t amount;                       // O(1) get amount
+    int8_t (*cmp_callback)(void*, void*);  // Comparsion function for binary search
 };
 
 bst_node_t* bst_node_create(void* data) {
@@ -40,10 +41,12 @@ bst_t* bst_create(int8_t (*cmp_callback)(void*, void*)) {
     return new_bst;
 }
 
+// Recurrsive helper use preorder to create a BST
 static bst_node_t* _bst_create_from_list(void** list, int64_t left, int64_t right) {
     if (left > right) {
         return NULL;
     }
+    // Make the middle to the root for every subtree
     int64_t middle = left + (right - left) / 2;
 
     bst_node_t* root = bst_node_create(list[middle]);
@@ -54,6 +57,7 @@ static bst_node_t* _bst_create_from_list(void** list, int64_t left, int64_t righ
     return root;
 }
 
+// API for create a BST from sorted list, a balance BST that search time is O(log n)
 bst_t* bst_create_from_list(void** list, uint64_t list_size, int8_t (*cmp_callback)(void*, void*)) {
     if (list == NULL) {
         return NULL;
@@ -76,6 +80,7 @@ void bst_insert(bst_t* bst, void* data, void (*equal_handle_callback)(void*, voi
     }
 
     bst_node_t* curr = bst->root;
+    // Iterative search to insert
     while (curr != NULL) {
         int8_t cmp_result = bst->cmp_callback(data, curr->data);
         if (cmp_result < 0) {
@@ -94,6 +99,7 @@ void bst_insert(bst_t* bst, void* data, void (*equal_handle_callback)(void*, voi
             curr = curr->right;
 
         } else {
+            // If caller want to handle or just skip
             if (equal_handle_callback != NULL) {
                 equal_handle_callback(curr, data);
             }
@@ -108,6 +114,7 @@ bst_node_t* bst_search(bst_t* bst, void* data) {
     }
 
     bst_node_t* curr = bst->root;
+    // Iterative search
     while (curr != NULL) {
         int8_t cmp_result = bst->cmp_callback(data, curr->data);
         if (cmp_result < 0) {
@@ -128,6 +135,7 @@ void bst_delete(bst_t* bst, void* data, void (*free_callback)(void**)) {
     bst_node_t* parent = NULL;
     bst_node_t* curr = bst->root;
     int8_t cmp_result;
+    // Iterative search the node that be deleted
     while (curr != NULL && (cmp_result = bst->cmp_callback(data, curr->data)) != 0) {
         parent = curr;
         if (cmp_result < 0) {
@@ -138,9 +146,12 @@ void bst_delete(bst_t* bst, void* data, void (*free_callback)(void**)) {
     }
 
     if (curr == NULL) {
-        return;
+        return;  // Not found
     }
+
+    // Conditon of two child
     if (curr->left != NULL && curr->right != NULL) {
+        // Find the successor (smallest of right subtree)
         bst_node_t* succ_parent = curr;
         bst_node_t* succ = curr->right;
         while (succ->left != NULL) {
@@ -148,18 +159,21 @@ void bst_delete(bst_t* bst, void* data, void (*free_callback)(void**)) {
             succ = succ->left;
         }
 
+        // Swap data
         free_callback(&(curr->data));
         curr->data = succ->data;
         succ->data = NULL;
 
+        // Move delete target to successor
         parent = succ_parent;
         curr = succ;
     }
 
     bst_node_t* child = (curr->left != NULL) ? curr->left : curr->right;
 
+    // Conditon of one child
     if (parent == NULL) {
-        bst->root = child;
+        bst->root = child;  // No parent means is root
     } else {
         if (curr == parent->left) {
             parent->left = child;
@@ -175,6 +189,7 @@ void bst_delete(bst_t* bst, void* data, void (*free_callback)(void**)) {
     bst->amount--;
 }
 
+// Recurrsive check
 bool _is_same_bst(bst_node_t* node1, bst_node_t* node2, bool (*is_same_callback)(void*, void*)) {
     if (node1 == NULL && node2 == NULL) {
         return true;
@@ -205,6 +220,7 @@ bool is_same_bst(bst_t* bst1, bst_t* bst2, bool (*is_same_callback)(void*, void*
     return _is_same_bst(bst1->root, bst2->root, is_same_callback);
 }
 
+// Use iterative with stack to create a sorted list
 void** bst_inorder_to_list(bst_t* bst) {
     if (bst == NULL || bst->root == NULL) {
         return NULL;
@@ -213,24 +229,29 @@ void** bst_inorder_to_list(bst_t* bst) {
     uint64_t bst_amount = bst_get_amount(bst);
     void** list = xmalloc(sizeof(*list) * bst_amount);
 
+    // Create a stack for iterative
     stack_t* stack = stack_create();
     bst_node_t* curr = bst->root;
 
+    // Inorder with stack
     while (is_stack_empty(stack) == false || curr != NULL || idx < bst_amount) {
+        // Push all left node to stack
         while (curr != NULL) {
             stack_push(stack, curr);
             curr = curr->left;
         }
         curr = stack_pop(stack);
 
+        // Add to list
         list[idx++] = curr->data;
 
         curr = curr->right;
     }
-    stack_free(&stack);
+    stack_free(&stack);  // Make sure to free stack
     return list;
 }
 
+// Use iterative with stack same as top
 void bst_inorder_func(bst_t* bst, void (*callback)(void*, void*), void* args) {
     if (bst == NULL || bst->root == NULL || callback == NULL) {
         return;
@@ -245,7 +266,10 @@ void bst_inorder_func(bst_t* bst, void (*callback)(void*, void*), void* args) {
             curr = curr->left;
         }
         curr = stack_pop(stack);
+
+        // Call function pass data and args
         callback(curr->data, args);
+
         curr = curr->right;
     }
     stack_free(&stack);
@@ -259,6 +283,7 @@ void bst_node_free(bst_node_t** node, void (*free_callback)(void**)) {
     xfree((*node));
 }
 
+// Recurrsive free
 static void _bst_free(bst_node_t** node, void (*free_callback)(void**)) {
     if (node == NULL || *node == NULL || free_callback == NULL) {
         return;
