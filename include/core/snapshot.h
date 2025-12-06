fff14c325a8c1c32e74774016a21d7086bc90254
Eender753030 <ender303030@gmail.com>
2025-12-06 23:01:03 +0800

    docs: Add comments for snapshot

--- include/core/snapshot.h
+++ include/core/snapshot.h
@@ -6,32 +6,48 @@
 
 #include "ds/bst.h"
 
+// Type of file_info that does not provide structure detail for client
 typedef struct file_info file_info_t;
 
+// Using snapshot_node_t to repersent bst_node_t from bst.h for specialized store snapshot data
 typedef bst_node_t snapshot_node_t;
 
+// Using snapshot_bst_t to repersent bst_t from bst.h for specialized is snapshot BST
 typedef bst_t snapshot_bst_t;
 
+// Direct create file_info by data
 file_info_t *file_info_create_from_index(const char *path, const char *hash, bool is_changed);
 
+// Create a empty snapshot BST
 snapshot_bst_t *snapshot_bst_create();
 
+// Create a balance snapshot BST from sorted list
 snapshot_bst_t *snapshot_bst_create_from_list(file_info_t **list, uint64_t size);
 
+// Insertion for snapshot BST that may have leader(HEAD) information
 void snapshot_bst_insert(snapshot_bst_t *self, const char *path, snapshot_bst_t *leader_bst);
 
+// Insertion for snapshot BST that store project directory datas
 void snapshot_bst_insert_projectdir(snapshot_bst_t *self, const char *path);
 
+// Insertion for snapshot BST that store directorys' path
 void snapshot_bst_insert_only_path(snapshot_bst_t *self, const char *path);
 
+// Deletion for snapshot BST
 void snapshot_bst_delete(snapshot_bst_t *self, const char *path);
 
+// Free all snapshot BST
 void snapshot_bst_free(snapshot_bst_t **bst);
 
+// Getter for file_info's data
 void file_info_get_content(file_info_t *file_info, char **path, char **hash, bool *is_changed);
 
+/* Check two file_info is the same
+ * Function for is_same_bst to comparsion
+ */
 bool is_same_file_info(void *file_info1, void *file_info2);
 
+// Check path whether in the snapshot BST
 bool is_snapshot_bst_contains(snapshot_bst_t *self, const char *path);
 
 #endif
--- src/core/snapshot.c
+++ src/core/snapshot.c
@@ -10,10 +10,13 @@
 #include "utils/memory.h"
 #include "utils/utils.h"
 
+// file_info structure
 struct file_info {
-    char *path;
-    char *hash;
-    bool is_changed;
+    char *path;  // file's path
+    char *hash;  // blob hash create by file
+
+    // (this variable is not used)
+    bool is_changed;  // record this file is change or not from previous commit
 };
 
 static file_info_t *file_info_create(const char *path, snapshot_bst_t *leader_bst) {
@@ -24,17 +27,19 @@ static file_info_t *file_info_create(const char *path, snapshot_bst_t *leader_bs
     file_info_t *new_file_info = xmalloc(sizeof(*new_file_info));
 
     new_file_info->path = str_dup(path);
-    new_file_info->hash = blob_create_from_file(path);
+    new_file_info->hash = blob_create_from_file(path);  // Create blob and get it's hash
 
     snapshot_node_t *leader_node = NULL;
+    // If now has leader(HEAD) commit, search it has the same path inside
     if (leader_bst != NULL && (leader_node = bst_search(leader_bst, new_file_info)) != NULL) {
         file_info_t *node_file_info = bst_node_get_data(leader_node);
+        // Compare hash check whether content is changed
         if (strcmp(new_file_info->hash, node_file_info->hash) == 0) {
             new_file_info->is_changed = false;
             return new_file_info;
         }
     }
-    new_file_info->is_changed = true;
+    new_file_info->is_changed = true;  // New file is changed
     return new_file_info;
 }
 
@@ -45,6 +50,7 @@ static file_info_t *file_info_create_from_projectdir(const char *path) {
 
     file_info_t *new_file_info = xmalloc(sizeof(*new_file_info));
 
+    // Just get hash without create blob
     new_file_info->path = str_dup(path);
     new_file_info->hash = blob_get_file_hash(path);
     new_file_info->is_changed = true;
@@ -77,18 +83,24 @@ file_info_t *file_info_create_from_index(const char *path, const char *hash, boo
     return new_file_info;
 }
 
+// Callback function for BST comparsion
+// Using path as base of comparsion to create snapshot BST
 static int8_t path_cmp(void *file_info1, void *file_info2) {
     return (int8_t)strcmp(((file_info_t *)file_info1)->path, ((file_info_t *)file_info2)->path);
 }
 
+// Empty snapshot BST with path comparsion function
 snapshot_bst_t *snapshot_bst_create() {
     return bst_create(path_cmp);
 }
 
+// Balance snapshot BST with path comparsion function
 snapshot_bst_t *snapshot_bst_create_from_list(file_info_t **list, uint64_t size) {
     return bst_create_from_list((void **)list, size, path_cmp);
 }
 
+// Callback function for handle equal situation when insert
+// Replace old hash with new hash
 static void equal_handle(void *node, void *new_file_info) {
     file_info_t *file_info = bst_node_get_data(node);
     xfree(file_info->hash);
@@ -129,6 +141,7 @@ void snapshot_bst_insert_only_path(snapshot_bst_t *self, const char *path) {
     bst_insert(self, new_file_info, equal_handle);
 }
 
+// Helper function to free file_info and it's data
 static void file_info_free(void **file_info) {
     xfree(((file_info_t *)*file_info)->path);
     xfree(((file_info_t *)*file_info)->hash);
@@ -153,6 +166,7 @@ void snapshot_bst_free(snapshot_bst_t **bst) {
     bst_free(bst, file_info_free);
 }
 
+// Getter that if caller need pass pointer otherwith NULL
 void file_info_get_content(file_info_t *file_info, char **path, char **hash, bool *is_changed) {
     if (file_info == NULL) {
         return;
